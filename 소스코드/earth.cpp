e2c3ee6c2df8c8d46c130cbaeb032465bb1618ad
JKMeen <rhkdals1206@naver.com>
2016-10-18 02:39:42 +0900

    update

--- 소스코드/earth.cpp
+++ 소스코드/earth.cpp
@@ -5,177 +5,345 @@
 #include <gl/glut.h>
 #include <math.h>
 
-float range = 1.0;
-float aspRatio = 1.0;
-float dX = 0.0;
-float rt[8] = {0, 0, 0, 0, 0, 0, 0};
-float srt[9] = {0, 0, 0, 0, 0, 0, 0};
+GLfloat range = 1.0;
+GLfloat aspRatio = 1.0;
+GLfloat dX = 0.0;
+GLfloat rt[8] = { 0, 0, 0, 0, 0, 0, 0 };
+GLfloat srt[9] = { 0, 0, 0, 0, 0, 0, 0 };
+GLint num = 8;
+GLfloat P_distance = 0, Prt;
 
-void drawAxis(){
-	glBegin(GL_LINES);
+////////////////////////////////////////////////
+// 마우스
+GLdouble angle = 0.0; // 회전 각
+GLfloat cx, cy, cz; // 클릭 좌표
+GLfloat ca; // 클릭 앵글
+// 마우스 이동량 배율
 
-	glColor3f(1, 0, 0);
-	glVertex3f(0, 0, 0);
-	glVertex3f(1, 0, 0);
+GLfloat red[] = { 0.8, 0.2, 0.2, 1.0 };
+GLfloat pos[] = { 3.0, 4.0, 5.0, 1.0 };
+GLdouble ex = 0.0, ey = 0.0, ez = 10.0;
+GLdouble tx = 0.0, ty = 0.0, tz = 0.0;
+GLdouble ax = 0.0, ay = 1.0, az; // 회전 축
 
-	glColor3f(0, 1, 0);
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, 1, 0);
+GLdouble sx, sy;
+#define SCALE 360.0
 
-	glColor3f(0, 0, 1);
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, 0, 1);
-	
-	glEnd();
+////////////////////////////////////////////////
+// 마지막 성분이 1이면 점광원이고, 0이면 방향광원(directional light source)
+
+//재질의 정반사, 난반사, 주변광, 반질거림 특성으로 사용될 데이터
+GLfloat matSpec[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat matDiff[] = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat matAmbi[] = { 0.5, 0.1, 0.1, 1.0 };
+GLfloat matShin[] = { 125.0 };
+
+// 광원의 정반사, 난반사, 주변광 특성으로 사용될 데이터
+//GLfloat light[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat lit_specular[] = { 1.0, 1.0f, 1.0f, 1.0f }; // 특정 방향으로 빛이 들어오고 특정 방향으로 반사GLfloat lit_diffuse[] = { 0.1, 1.0f, 1.0f, 1.0f };  // 모든 방향으로 균일하게 반사GLfloat lit_ambient[] = { 0.5, 1.0f, 1.0f, 1.0f };  // 특정방향을 가지지 않는 빛
+
+// 광원의 위치로 사용될 데이터
+GLfloat lightPos[] = { 0.0, 0.0, 0.0, 0.0 };//{ -1.0, 1.0, 1.0, 1.0 };
+
+////////////////////////////////////////////////
+
+// 조명과 재질의 특성을 준비된 데이터로 설정하는 함수
+void LightSet() {
+	glMaterialfv(GL_FRONT, GL_SPECULAR, matSpec);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, matDiff);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, matAmbi);
+	glMaterialfv(GL_FRONT, GL_SHININESS, matShin);
+
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lit_specular);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lit_diffuse);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lit_ambient);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 }
 
-void myDisplay() {
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(2, 2, 2, 0, 0, 0, 0, 1, 0);
-	
-	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-	/*
-	glLineWidth(5);
-	drawAxis();
-	glLineWidth(1);
-	*/
+// 조명의 위치를 설정하는 함수
+void LightPosition() {
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+}
 
-	
+void Initialize(){
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f);
+	glShadeModel(GL_SMOOTH); // 매끄러운 세이딩
+	glEnable(GL_DEPTH_TEST); // 가려진 몇 제거
+	glEnable(GL_CULL_FACE); // 후면 제거
+	glFrontFace(GL_CCW); // 다각형을 반시계방향으로 감음
 
-	for (int i=0; i<=7; i++){ // 공전
-		switch(i){
-			case 0: rt[i]+=0.4726; break;  // 수성   / 반지름 2500  / 공전 88일   / 자전 59일        / 위성 x
-			case 1: rt[i]+=0.3502; break;  // 금성   / 반지름 6000  / 공전 225일  / 자전 223일       / 위성 x
-			case 2: rt[i]+=0.2978; break;  // 지구   / 반지름 6400  / 공전 365일  / 자전 1일         / 위성 1개
-			case 3: rt[i]+=0.2408; break;  // 화성   / 반지름 3400  / 공전 687일  / 자전 24시간 37분 / 위성 2개
-			case 4: rt[i]+=0.1306; break;  // 목성   / 반지름 71000 / 공전 11.9년 / 자전 9시간 50분  / 위성 16개
-			case 5: rt[i]+=0.0965; break;  // 토성   / 반지름 60000 / 공전 29.5년 / 자전 10시간 14분 / 위성 23개/ 고리
-			case 6: rt[i]+=0.0681; break;  // 천왕성 / 반지름 27900 / 공전 84년   / 자전 15시간      / 위성 15개 / 고리
-			case 7: rt[i]+=0.0544; break;  // 해왕성 / 반지름 24300 / 공전 165년  / 자전 15시간 18분 / 위성 5개 / 고리
-		}                                        // 달 / 반지름 1700 / 공전 30일 / 자전 273일
+	glEnable(GL_LIGHTING); // 조명 활성화
+
+	// LIGHT0 에 대한 재질 설정
+	glMaterialfv(GL_FRONT, GL_AMBIENT, 1.0);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, 1.0);
+
+	// LIGHT0 설정
+	glLightfv(GL_LIGHT0, GL_AMBIENT, 0.5); // 주변광 세기
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, 0.5); // 분산광 세기
+	glLightfv(GL_LIGHT0, GL_POSITION, 0.0); // 광원 위치
+}
+
+void special(int key, int x, int y)
+{
+	switch (key) {
+	case GLUT_KEY_UP:
+		num -= 1;
+		if (num <= 0) num = 0;
+		break;
+	case GLUT_KEY_DOWN:
+		num += 1;
+		if (num >= 8) num = 8;
+		break;
+	case GLUT_KEY_LEFT:
+		num += 1;
+		if (num >= 8) num = 8;
+		break;
+	case GLUT_KEY_RIGHT:
+		num += 1;
+		if (num >= 8) num = 8;
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay(); // 윈도우를 그려야 할 때만 그려줌 (glutIdleFunc를 안써도됨)
+}
+
+void motion(int x, int y){
+	double dx, dy, a;
+
+	// 마우스 포인터 위치의 끌기 시작 위치에서의 변위
+	dx = (x - cx) * sx;
+	dy = (y - cy) * sy;
+
+	// 마우스 포인터 위치의 끌기 시작 위치에서의 거리
+	a = sqrt(dx * dx + dy * dy);
+
+	if (a != 0.0){
+		// 거리를 각도로 환산하여 드래그 시작시의 회전 각에 가산
+		angle = fmod(ca + SCALE * a, 360.0);
+
+		// 마우스 포인터의 변위에서 회전축 벡터를 요청
+		ax = dy / a;
+		ay = dx / a;
+		az = 0.0;
+
+		// 도형의 재 묘화
+		glutPostRedisplay();
+	}
+}
+
+void mouse(int button, int direction, int x, int y)
+{
+	switch (direction){
+	case GLUT_DOWN:
+		// 마우스 버튼을 누른 위치를 기록
+		cx = x;
+		cy = y;
+		// 표시하고 있는 물체의 회전 각을 기록
+		ca = angle;
+		break;
+	default:
+		break;
+	}
+}
+
+void drawPlanet(float R, float G, float B, float Revolution, float Rx, float Ry, float Rz, float distance, float rotation, float rx, float ry, float rz, float radius){
+	glColor3f(R, G, B);
+	glRotatef(Revolution, Rx, Ry, Rz); // 공전
+	glTranslatef(distance, 0, 0); // 태양과의 거리
+	glRotatef(rotation, rx, ry, rz); // 자전
+	//glutWireSphere(radius, 20, 20); 
+	glutSolidSphere(radius, 20, 20); // 스페어 (반지름, 경도, 위도)
+
+	// 위성
+	if (num >= 3){
+		glRotatef(-srt[num], 1, 0, 0);
+		glColor3f(1, 1, 0);
+		glRotatef(srt[num], 0, 1, 0);
+		glTranslatef(distance / 10, 0, 0);
+		glRotatef(srt[num], 0, 1, 0);
+		glRotatef(srt[num], 0, 0, 1);
+		glutSolidSphere(radius / 10, 10, 10); // 스페어 (반지름, 경도, 위도)
+	}
+}
+
+void drawMoon(int num){ // 달
+	glPushMatrix();
+	glRotatef(-srt[num], 1, 0, 0);
+	glColor3f(1, 1, 0);
+	glRotatef(srt[num], 0, 1, 0);
+	glTranslatef(0.5, 0, 0);
+	glRotatef(srt[num], 0, 1, 0);
+	glRotatef(srt[num], 0, 0, 1);
+	glutSolidSphere(0.17, 10, 10); // 스페어 (반지름, 경도, 위도)
+}
+
+void displayPlanet(){
+	// 태양()
+	glPushMatrix();
+	glColor3f(1, 0.5, 0);
+	glRotatef(srt[0], 0, 1, 0); // 자전
+	//glutWireSphere(radius, 20, 20); 
+	glutWireSphere(27*2, 20, 20); // 스페어 (반지름, 경도, 위도)
+	//drawPlanet(1, 0, 0, 0, 0, 0, 0, 0, srt[0], 0, 1, 0, 27 * 2);
+	glPopMatrix();
+
+	// 수성(Mercury)
+	glPushMatrix();
+	glColor3f(0, 0, 1);
+	drawPlanet(0, 0, 1, rt[0], 0, 1, 0, 57.9, srt[1], 1, 0, 0, 0.25 * 10);
+	glPopMatrix();
+
+	// 금성(Venus) 
+	glPushMatrix();
+	drawPlanet(1, 1, 0, rt[1], 0, 1, 0, 108.2, srt[2], 1, 0, 0, 0.6 * 10);
+	glPopMatrix();
+
+	// 지구(Earth)
+	glPushMatrix();
+	drawPlanet(0, 1, 0, rt[2], 0, 1, 0, 149.6, srt[3], 1, 0, 0, 0.64 * 10);
+	glPopMatrix();
+
+	// 화성(Mars)
+	glPushMatrix();
+	drawPlanet(1, 0, 0, rt[3], 0, 1, 0, 228.0, srt[4], 1, 0, 0, 0.34 * 10);
+	glPopMatrix();
+
+	// 목성(Jupiter)
+	glPushMatrix();
+	drawPlanet(0.5, 0, 0, rt[4], 0, 1, 0, 778.3, srt[5], 1, 0, 0, 7.1 * 10);
+	glPopMatrix();
+
+	// 토성(Saturn)
+	glPushMatrix();
+	drawPlanet(1, 0.5, 0, rt[5], 0, 1, 0, 1427.0, srt[6], 1, 0, 0, 6.0 * 10);
+	glPopMatrix();
+
+	// 천왕성(Uranus)
+	glPushMatrix();
+	drawPlanet(0, 0, 0.7, rt[6], 0, 1, 0, 2900.0, srt[7], 1, 0, 0, 2.79 * 10);
+	glPopMatrix();
+
+	// 해왕성(Neptune)
+	glPushMatrix();
+	drawPlanet(0, 0, 0.9, rt[7], 0, 1, 0, 4497.0, srt[8], 1, 0, 0, 2.43 * 10);
+	glPopMatrix();
+}
+
+void setRotation(){
+	// 수성(Mercury)   / 반지름 2500  / 공전 88일(4736)   / 자전 59일        / 위성 x
+	// 금성(Venus)     / 반지름 6000  / 공전 225일(3502)  / 자전 223일       / 위성 x
+	// 지구(Earth)     / 반지름 6400  / 공전 365일(2978)  / 자전 1일         / 위성 1개
+	// 화성(Mars)      / 반지름 3400  / 공전 687일(2408)  / 자전 24시간 37분 / 위성 2개
+	// 목성(Jupiter)   / 반지름 71000 / 공전 11.9년(1306) / 자전 9시간 50분  / 위성 16개
+	// 토성(Saturn)    / 반지름 60000 / 공전 29.5년(965)  / 자전 10시간 14분 / 위성 23개/ 고리
+	// 천왕성(Uranus)  / 반지름 27900 / 공전 84년(681)    / 자전 15시간      / 위성 15개 / 고리
+	// 해왕성(Neptune) / 반지름 24300 / 공전 165년(544)   / 자전 15시간 18분 / 위성 5개 / 고리
+	//* 달 / 반지름 1700 / 공전 30일 / 자전 273일
+	for (int i = 0; i <= 7; i++){ // 공전
+		switch (i){
+		case 0: rt[i] += 0.4736; break;
+		case 1: rt[i] += 0.3502; break;
+		case 2: rt[i] += 0.2978; break;
+		case 3: rt[i] += 0.2408; break;
+		case 4: rt[i] += 0.1306; break;
+		case 5: rt[i] += 0.0965; break;
+		case 6: rt[i] += 0.0681; break;
+		case 7: rt[i] += 0.0544; break;
+		}
 		if (rt[i] >= 360){
 			rt[i] = 0;
 		}
 	}
 
-	for (int i=0; i<=8; i++){ // 자전
-		switch(i){
-			case 0: srt[i]+= 0.4726; break;  // 태양
-			case 1: srt[i]+= 0.2409; break;  // 수성
-			case 2: srt[i]+=0.6102; break;  // 금성
-			case 3: srt[i]+=1.0; break;  // 지구
-			case 4: srt[i]+=1.8809; break;  // 화성
-			case 5: srt[i]+=11.862; break;  // 목성
-			case 6: srt[i]+=29.458; break;  // 토성
-			case 7: srt[i]+=84.022; break;  // 천왕성
-			case 8: srt[i]+=164.774; break;  // 해왕성
+	for (int i = 0; i <= 8; i++){ // 자전
+		switch (i){
+		case 0: srt[i] += 0.59; break;  // 태양
+		case 1: srt[i] += 0.59; break;  // 수성
+		case 2: srt[i] += 0.223; break;  // 금성
+		case 3: srt[i] += 1.0; break;  // 지구
+		case 4: srt[i] += 1.1; break;  // 화성
+		case 5: srt[i] += 2.5; break;  // 목성
+		case 6: srt[i] += 2.3; break;  // 토성
+		case 7: srt[i] += 4.16; break;  // 천왕성
+		case 8: srt[i] += 4.162; break;  // 해왕성
 		}
 		if (srt[i] >= 360){
 			srt[i] = 0;
 		}
 	}
 
-	// 태양
-	glPushMatrix();
-	glColor3f(1, 0, 0);
-	glRotatef(srt[0], 0, 1, 0);
-	glutWireSphere(27, 20, 20); // 스페어 (반지름, 경도, 위도)
-	glPopMatrix();
+	switch (num){
+	case 0: Prt = 0;  P_distance = 27.5; break;
+	case 1: Prt = rt[0];  P_distance = 57.9 + 1; break;
+	case 2: Prt = rt[1];  P_distance = 108.2 + 1; break;
+	case 3: Prt = rt[2];  P_distance = 149.6 + 1; break;
+	case 4: Prt = rt[3];  P_distance = 228.0 + 1; break;
+	case 5: Prt = rt[4];  P_distance = 778.3 + 8; break;
+	case 6: Prt = rt[5];  P_distance = 1427.0 + 7; break;
+	case 7: Prt = rt[6];  P_distance = 2900.0 + 3; break;
+	case 8: Prt = rt[7];  P_distance = 4497.0 + 3; break;
+	}
+}
 
-	// 수성
-	glPushMatrix();
-	glColor3f(0, 0, 1);
-	glRotatef(srt[1], 0, 1, 0);
-	glTranslatef(10, 0, 0); // 태양과의 거리
-	glRotatef(srt[1], 1, 0, 0);
-	glutWireSphere(0.25, 10, 10); // 스페어 (반지름, 경도, 위도)
-	glPopMatrix();
+void myDisplay() {
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	//카메라 설정
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-P_distance, P_distance, -P_distance, P_distance, -P_distance, P_distance);
+	gluPerspective(0, aspRatio, 0.1, 10000);
 
-	// 금성
-	glPushMatrix();
-	glColor3f(1, 1, 0);
-	glRotatef(srt[2], 0, 1, 0);
-	glTranslatef(20, 0, 0); // 태양과의 거리
-	glRotatef(srt[2]*2, 1, 0, 0);
-	glutWireSphere(0.6, 10, 10); // 스페어 (반지름, 경도, 위도)
-	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(3*cos(10.0), 1, 3*sin(10.0), 0, 0, 0, 0, 1, 0);
 
-	// 지구
-	glPushMatrix();
-	glColor3f(0, 1, 0);
-	glRotatef(srt[3], 0, 1, 0);
-	glTranslatef(30, 0, 0); // 태양과의 거리
-	glRotatef(srt[3]*2, 1, 0, 0);
-	glutWireSphere(0.64, 10, 10); // 스페어 (반지름, 경도, 위도)
-	  // 달
-	glRotatef(-srt[3], 1, 0, 0);
-	glColor3f(1, 1, 1);
-	glRotatef(srt[3], 0, 1, 0);
-	glTranslatef(0.5, 0, 0);
-	glRotatef(srt[3], 0, 1, 0);
-	glRotatef(srt[3], 0, 0, 1);
-	glutWireSphere(0.17, 10, 10); // 스페어 (반지름, 경도, 위도)
-	glPopMatrix();
+	// 빛 설정
+	LightPosition();
+	glDisable(GL_LIGHTING);
 
-	// 화성
-	glPushMatrix();
-	glColor3f(1, 0, 0);
-	glRotatef(srt[4], 0, 1, 0);
-	glTranslatef(40, 0, 0); // 태양과의 거리
-	glRotatef(srt[4]*2, 1, 0, 0);
-	glutWireSphere(0.34, 10, 10); // 스페어 (반지름, 경도, 위도)
-	  // 위성1
-	glRotatef(-srt[4], 1, 0, 0);
-	glColor3f(1, 1, 1);
-	glRotatef(srt[4], 0, 1, 0);
-	glTranslatef(0.5, 0, 0);
-	glRotatef(srt[4], 0, 1, 0);
-	glRotatef(srt[4], 0, 0, 1);
-	glutWireSphere(0.17, 10, 10); // 스페어 (반지름, 경도, 위도)
-	  // 위성2
-	glRotatef(-srt[4], 1, 0, 0);
-	glColor3f(1, 1, 1);
-	glRotatef(srt[4], 0, 1, 0);
-	glTranslatef(0.5, 0, 0);
-	glRotatef(srt[4], 0, 1, 0);
-	glRotatef(srt[4], 0, 0, 1);
-	glutWireSphere(0.17, 10, 10); // 스페어 (반지름, 경도, 위도)
-	glPopMatrix();
-	
-	// 목성
-	glPushMatrix();
-	glColor3f(0.5, 0, 0);
-	glRotatef(srt[4], 0, 1, 0);
-	glTranslatef(80, 0, 0); // 태양과의 거리
-	glRotatef(srt[4]*2, 1, 0, 0);
-	glutWireSphere(7.1, 10, 10); // 스페어 (반지름, 경도, 위도)
-		 // 위성1
-	//for (int i=0; i<16; i++){
-		glRotatef(-srt[4], 1, 0, 0);
-		glColor3f(1, 1, 1);
-		glRotatef(srt[4], 0, 1, 0);
-		glTranslatef(0.5, 0, 0);
-		glRotatef(srt[4], 0, 1, 0);
-		glRotatef(srt[4], 0, 0, 1);
-		glutWireSphere(0.17, 10, 10); // 스페어 (반지름, 경도, 위도)
-	//}
-	glPopMatrix();
+	// 마우스 클릭 - 회전
+	glRotated(angle, ax, ay, 0.0);
 
+	// 공전 / 자전 설정
+	setRotation();
+
+	// 빛 실행
+	glEnable(GL_LIGHTING);
+
+	// 각 행성 디스플레이
+	displayPlanet();
+
+	// 스왑 버퍼
 	glutSwapBuffers();
 }
 
 void SetCamera() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-100, 100, -100, 100, -100, 100);
-	gluPerspective(0, aspRatio, 0.1, 1000);
+	glOrtho(-P_distance, P_distance, -P_distance, P_distance, -P_distance, P_distance);
+	gluPerspective(0, aspRatio, 0.01, 50000);
 }
 
 void reshape(int w, int h){
-	aspRatio = float(w)/h;
-	SetCamera();
+	aspRatio = float(w) / h;
+	//SetCamera();
 	glViewport(0, 0, w, h);
+
+	// 마우스 포인터 위치 윈도우의 상대적인 위치에 환율 용
+	sx = 1.0 / (double)w;
+	sy = 1.0 / (double)h;
+}
+
+void GLInit() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	// 조명의 특성과 재질의 특성을 설정
+	LightSet();
+	glEnable(GL_DEPTH_TEST);
 }
 
 int main(int argc, char **argv) {
@@ -183,14 +351,17 @@ int main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(512, 512);
-	glutCreateWindow("12510096 조광민");
-	glEnable(GL_DEPTH_TEST);
+	glutCreateWindow("12510096 조광민 Sun");
 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	
+	GLInit();
+
 	glutDisplayFunc(myDisplay);
 	glutIdleFunc(myDisplay);
 	glutReshapeFunc(reshape);
+
+	glutSpecialFunc(special); // 키보드 특수 키
+	glutMouseFunc(mouse); // 마우스 클릭 시 발생되는 이벤트
+	glutMotionFunc(motion); // 마우스 클릭 후 이동 시 발생되는 이벤트
 
 	glutMainLoop();
 
--- 소스코드/pingpong.cpp
+++ 소스코드/pingpong.cpp
@@ -8,11 +8,11 @@
 #include <time.h>
 #include <stdio.h>
 
-float movex=0, shoot=-1;
-float ballx =0.003, bally =0.001, changex= -1, changey= -1;
+float movex = 0, shoot = -1;
+float ballx = 0.003, bally = 0.001, changex = -1, changey = -1;
 int xspeed, yspeed;
 float blockx[9], blocky[9];
-int k = 0, delay = 0, block[9]={0,0,0,0,0,0,0,0,0};
+int k = 0, delay = 0, block[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void special(int key, int x, int y){
 	switch (key){
@@ -41,7 +41,8 @@ void drawObject(int s){
 		glVertex3f(0.9, 0.9, 0);
 		glVertex3f(-0.9, 0.9, 0);
 		glEnd();
-	} else if (s == 1){ // Player
+	}
+	else if (s == 1){ // Player
 		glBegin(GL_LINE_LOOP);
 		glColor3f(0, 0, 1);
 		glVertex3f(-0.2, -0.75, 0);
@@ -49,7 +50,8 @@ void drawObject(int s){
 		glVertex3f(0.2, -0.7, 0);
 		glVertex3f(-0.2, -0.7, 0);
 		glEnd();
-	} else if (s == 2){ // Ball
+	}
+	else if (s == 2){ // Ball
 		glBegin(GL_LINE_LOOP);
 		int Points = 20;
 		float radius = 0.05;
@@ -63,22 +65,23 @@ void drawObject(int s){
 		glEnd();
 	}
 }
+
 void drawBlock(float x, float y){
 	glBegin(GL_LINE_LOOP);
 	glColor3f(1, 0, 0);
-	glVertex3f(-0.15+x, y, 0);
-	glVertex3f(0.15+x, y, 0);
-	glVertex3f(0.15+x, y+0.05, 0);
-	glVertex3f(-0.15+x, y+0.05, 0);
+	glVertex3f(-0.15 + x, y, 0);
+	glVertex3f(0.15 + x, y, 0);
+	glVertex3f(0.15 + x, y + 0.05, 0);
+	glVertex3f(-0.15 + x, y + 0.05, 0);
 	glEnd();
 }
 
 void myDisplay(){
-
+	
 	if (delay == 0){
 		srand((unsigned int)time(NULL));
-		xspeed = rand() % 4+2;
-		yspeed = rand() % 3+2;
+		xspeed = rand() % 4 + 2;
+		yspeed = rand() % 3 + 2;
 		delay = 1;
 	}
 
@@ -86,23 +89,23 @@ void myDisplay(){
 
 	if (ballx >= 0.85 || ballx <= -0.85) // 좌,우 충돌
 		changex *= -1;
-	if (bally >=0.85) // 천장 충돌
+	if (bally >= 0.85) // 천장 충돌
 		changey = -1;
 	if (bally <= -0.85) // 바닥 충돌 (점수--)
 		changey = 1;
-	if (ballx <= movex+0.2 && ballx >= movex-0.2 && bally <= -0.65) // 플레이어 충돌
+	if (ballx <= movex + 0.2 && ballx >= movex - 0.2 && bally <= -0.65) // 플레이어 충돌
 		changey = 1;
 
-	ballx += 0.0001*xspeed*changex; 
-	bally += 0.0001*yspeed*changey;
+	ballx += 0.0001*changex;
+	bally += 0.0001*changey;
 
 	// Room
-	drawObject(0); 
-	
+	drawObject(0);
+
 	// Player
 	glPushMatrix();
 	glTranslatef(movex, 0, 0);
-	drawObject(1); 
+	drawObject(1);
 	glPopMatrix();
 
 	// Ball
@@ -110,48 +113,7 @@ void myDisplay(){
 	glTranslatef(ballx, bally, 0);
 	drawObject(2);
 	glPopMatrix();
-
-	// block
-	for (int i=0; i<3; i++){
-		for(int j=0; j<3; j++){
-			if (block[k] != 1){
-				glPushMatrix();
-				blockx[k]= -0.6+0.6*j;
-				blocky[k]= 0.8-0.1*i;
-				glTranslatef(blockx[k], blocky[k], 0);
-				drawBlock(0,0);
-				glPopMatrix();
-				if (k < 9){
-					k++;
-				}
-			} else {
-				glPushMatrix();
-				blockx[k]= -0.6+0.6*j;
-				blocky[k]= -1-0.1*i;
-				glTranslatef(blockx[k], blocky[k], 0);
-				drawBlock(0,0);
-				glPopMatrix();
-				if (k < 9){
-					k++;
-				}
-			}
-		}
-	}
 	
-	for (int i=0; i<9; i++){
-		if (bally >= blocky[i]-0.05 && bally <= blocky[i]+0.05 && ballx >= blockx[i]-0.15 && ballx <= blockx[i]+0.15){ // 블록 충돌
-			changey = -1;
-			block[i]=1;
-		}
-	}
-
-	// block2
-	glPushMatrix();
-	blockx[1]= -0.6;
-	blocky[1]= 0.8;
-	glTranslatef(blockx[1], blocky[1], 0);
-	drawBlock(0,0);
-	glPopMatrix();
 	glFlush();
 }
 
