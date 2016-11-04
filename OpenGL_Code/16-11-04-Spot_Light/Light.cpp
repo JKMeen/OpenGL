4a983e6ebc1d3894eb0f3568800d451996295eae
JKMeen <rhkdals1206@naver.com>
2016-11-04 12:29:47 +0900

    update

---
+++ OpenGL_Code/16-11-04-Spot_Light/Light.cpp
@@ -0,0 +1,34 @@
+#include "Light.h"
+
+GLfloat Light_lightPos[] = { 1, 1, 1, 1 };
+
+void SetMaterial(float r, float g, float b) {
+	GLfloat diffuse[] = { r, g, b, 1.0 };
+	GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+	glMaterialf(GL_FRONT, GL_SHININESS, 100.0);
+}
+
+void SetLight(float r, float g, float b) {
+	GLfloat diffuse[] = { r, g, b, 1.0 };
+	GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+}
+
+void SetSpot(bool flag, float cutoff_angle){
+	if (!flag) glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 180.0);
+	else {
+		GLfloat direction[] = { -Light_lightPos[0], -Light_lightPos[1], -Light_lightPos[2] };
+		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, cutoff_angle);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, direction);
+	}
+}
+
+void SetLightPosition(float pos[]) {
+	glLightfv(GL_LIGHT0, GL_POSITION, pos);
+	for (int i = 0; i < 4; i++) Light_lightPos[i] = pos[i];
+}
